332b9c72a2cba575cfe214c7c9088e0a91298db0
Torstein <torstein.holmberget@gmail.com>
2018-10-08 12:00:34 +0200

    8.10.18
    update 3

--- Programmering/Øvinger/Øving 7/Temperaturer.java
+++ Programmering/Øvinger/Øving 7/Temperaturer.java
@@ -1,3 +1,12 @@
+/*
+*   Programmeringsoppgave 2 side 314
+*/
+
 class Temperaturer {
-    
+
+    public Temperaturer(){
+        this.dager = dager;
+        this.timer = timer;
+
+    }
 }
--- Programmering/Øvinger/Øving 8/NyString.java
+++ Programmering/Øvinger/Øving 8/NyString.java
@@ -1,14 +1,5 @@
 /*
 *   Programmeringsoppgave 1 side 283
-*   Lag en klasse NyString, den skal ha en objektvariabel av klassen String.
-*   Klassen NyString skal være immutabel og tilby følgende:
-*
-*   Konstruktør som tar et objekt av klassen String som argument.
-*
-*   Forkorting: det første tegnet i hvert ord skal plukkes ut, resultatet skal kunne hentes.
-*   (tips, bruk split() som returnerer en tabell med ordene som teksten består av: String[] ord = tekst.split("");)
-*
-*   Fjerning av tegn. (Tips, bruk en while løkke og for eksempel String-metodene indexOf() og substring())
 */
 
 class NyString {
