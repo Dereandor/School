528209edbdb6c9cf8a3e3a0925a4ea965ea03de8
Torstein <torstein.holmberget@gmail.com>
2018-10-10 22:29:08 +0200

    18-10-10
    update 1

--- Programmering/Øvinger/Øving 7/Temperaturer.java
+++ Programmering/Øvinger/Øving 7/Temperaturer.java
@@ -2,6 +2,8 @@
 *   Programmeringsoppgave 2 side 314
 */
 import java.util.Random;
+import java.util.Arrays;
+import java.text.DecimalFormat;
 
 class Temperaturer {
 
@@ -13,15 +15,109 @@ class Temperaturer {
     public Temperaturer(String navn, int dager, int timer){
         this.navn = navn;
         this.temperatur = new int[dager][timer];
-        setTemp(timer);
+        setTemp();
     }
 
-    public void setTemp(int timer) {
-        for(int i = 0; i < ; i++) {
-            int temp = tall.nextInt(25);
-            temp -= 10;
-
+    public void setTemp() {
+        for(int i = 0; i < temperatur.length; i++) {
+            for(int j = 0; j < temperatur[i].length; j++) {
+                int temp = tall.nextInt(25);
+                temp -= 10;
+                temperatur[i][j] += temp;
+            }
         }
     }
-    //middeltemperatur for hver dag i måneden
+    //gjennomsnittstemperaturen for spesifikk dag i måneden
+    public double getAverage(int dag) {
+        double average = 0;
+        int hourCount = 0;
+        for(int i = 0; i < temperatur[(dag-1)].length; i++) {
+            average += temperatur[(dag-1)][i];
+            hourCount++;
+        }
+        double result = average / hourCount;
+        return result;
+    }
+    //gjennomsnittstemperaturen for hver dag i måneden i egen tabell
+    public double[] getAverageDay() {
+        double[] averageDay = new double[temperatur.length];
+        double temp;
+        int hourCount;
+        //double average = 0;
+        for(int i = 0; i < temperatur.length; i++) {
+            double average;
+            temp = 0;
+            hourCount = 0;
+            for(int j = 0; j < temperatur[i].length; j++) {
+                temp += temperatur[i][j];
+                hourCount++;
+            }
+            average = temp / hourCount;
+            averageDay[i] = average;
+        }
+        return averageDay;
+    }
+    //gjennomsnittstemperaturen for hver time i måneden
+    public double[] getAverageHour() {
+        double[] averageHour = new double[temperatur[0].length];
+        double temp;
+        int dayCount;
+        for(int i = 0; i < temperatur[0].length; i++) {
+            double average;
+            temp = 0;
+            dayCount = 0;
+            for(int j = 0; j < temperatur.length; j++) {
+                temp += temperatur[j][i];
+                dayCount++;
+            }
+            average = temp / dayCount;
+            averageHour[i] += average;
+        }
+        return averageHour;
+    }
+    //gjennomsnittstemperaturen for hele måneden
+    public double getAverageMonth() {
+        double tempHour;
+        int hourCount;
+        int dayCount = 0;
+        double averageMonth = 0;
+        double averageDay = 0;
+        for(int i = 0; i < temperatur.length; i++) {
+            double averageHour;
+            tempHour = 0;
+            hourCount = 0;
+            for(int j = 0; j < temperatur[i].length; j++) {
+                tempHour += temperatur[i][j];
+                hourCount++;
+            }
+            averageHour = tempHour / hourCount;
+            averageDay += averageHour;
+            dayCount++;
+        }
+        averageMonth = averageDay / dayCount;
+        return averageMonth;
+    }
+    //antall døgn med gjennomsnittstemperatur: <-5(0), -5 til 0(1), 0 til 5(2), 5 til 10(3) og >10(4)
+    public double[] getAverageDays() {
+        double[] averageMonth = new double[5];
+        double[] averageDays = getAverageDay();
+            for (int i = 0; i < averageDays.length; i++) {
+                if (averageDays[i] <= -5) {
+                    averageMonth[0] += 1.0;
+                } else if (averageDays[i] > -5 && averageDays[i] <= 0) {
+                    averageMonth[1] += 1.0;
+                } else if (averageDays[i] > 0 && averageDays[i] <= 5) {
+                    averageMonth[2] += 1.0;
+                } else if (averageDays[i] > 5 && averageDays[i] <= 10) {
+                    averageMonth[3] += 1.0;
+                } else {
+                    averageMonth[4] += 1.0;
+                }
+            }
+        return averageMonth;
+    }
+
+    public String toString() {
+        return Arrays.deepToString(temperatur).replace("], ", "]\n").replace("[[", "[").replace("]]", "]");
+    }
 }
--- Programmering/Øvinger/Øving 7/TemperaturerKlient.java
+++ Programmering/Øvinger/Øving 7/TemperaturerKlient.java
@@ -1,7 +1,30 @@
+import static javax.swing.JOptionPane.*;
+import java.util.Arrays;
+
 class TemperaturerKlient {
 
     public static void main(String[]args) {
 
-        Temperaturer oktober = new Temperaturer(oktober, 31, 24);
+        //oprett ny månedstabell
+        Temperaturer oktober = new Temperaturer("oktober", 4, 4);
+
+        //hente ut hele tabellen
+        System.out.println("Hele tabellen: \n" + oktober.toString());
+
+        //hente gjennomsnittstemperaturen for spesifikk dag
+        System.out.println("Snitt for valgt dag: \n" + oktober.getAverage(1));
+
+        //hente ut gjennomsnittstemperaturen for hver dag
+        System.out.println("Snitt for hver dag: \n" + Arrays.toString(oktober.getAverageDay()));
+
+        //hente ut gjennomsnittstemperaturen for hver time
+        System.out.println("Snitt for hver time: \n" + Arrays.toString(oktober.getAverageHour()));
+
+        //hente ut gjennomsnittet for hele måneden
+        System.out.println("Snitt for hele måneden: \n" + oktober.getAverageMonth());
+
+        //hente ut ant. dager med gjennomsnittstemperatur: <-5(0), -5 til 0(1), 0 til 5(2), 5 til 10(3) og >10(4)
+        System.out.println("Antall dager med oppgitt temp: \n" + Arrays.toString(oktober.getAverageDays()));
+
     }
 }
