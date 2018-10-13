de5e305c1e733f36829b2d1a23010a86580930f1
Torstein <torstein.holmberget@gmail.com>
2018-10-13 17:52:30 +0200

    18-13-10

--- Programmering/Øvinger/Øving 7/Smiley.java
+++ Programmering/Øvinger/Øving 7/Smiley.java
@@ -4,7 +4,9 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 class Vindu extends JFrame {
+
     public Vindu(String tittel) {
+
         setTitle(tittel);
         setSize(400, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -15,6 +17,19 @@ class Vindu extends JFrame {
 
 class Tegning extends JPanel {
 
+    public void paintComponent(Graphics tegneflate) {
+
+        super.paintComponent(tegneflate);
+        setBackground(Color.RED);
+        tegneflate.setColor(Color.YELLOW);
+        tegneflate.fillOval(30, 30, 300, 300);
+        tegneflate.setColor(Color.BLACK);
+        tegneflate.fillOval(90, 90, 40, 40);
+        tegneflate.setColor(Color.BLACK);
+        tegneflate.fillOval(230, 90, 40, 40);
+        tegneflate.setColor(Color.BLACK);
+        tegneflate.fillRoundRect(90, 210, 180, 40, 40, 40);
+    }
 }
 
 
@@ -22,5 +37,7 @@ class Smiley {
 
     public static void main(String[]args) {
 
+        Vindu etVindu = new Vindu("Smiley");
+        etVindu.setVisible(true);
     }
 }
