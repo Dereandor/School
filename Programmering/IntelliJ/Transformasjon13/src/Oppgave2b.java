f3dac6d9ddb2091b722ece297ae41c3ed128cea4
Torstein <torstein.holmberget@gmail.com>
2018-12-18 09:45:42 +0100

    2018-12-18

---
+++ Programmering/IntelliJ/Transformasjon13/src/Oppgave2b.java
@@ -0,0 +1,97 @@
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.glu.GLU;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import com.jogamp.opengl.util.gl2.GLUT;
+
+import com.jogamp.opengl.util.FPSAnimator;
+
+public class Oppgave2b  extends GLCanvas implements GLEventListener {
+    
+    private static String TITLE = "Transformasjon 13";
+    private static final int CANVAS_WIDTH = 1280;  // width of the drawable
+    private static final int CANVAS_HEIGHT = 960; // height of the drawable
+    
+    // Setup OpenGL Graphics Renderer
+    private GLU glu;  // for the GL Utility
+    
+    /** Constructor to setup the GUI for this Component */
+    public Oppgave2b() {
+        this.addGLEventListener(this);
+    }
+
+// ------ Implement methods declared in GLEventListener (init,reshape,display,dispose)
+    
+    /**
+     * Called immediately after the OpenGL context is initialized. Can be used
+     * to perform one-time initialization. Run only once.
+     */
+    public void init(GLAutoDrawable drawable) {
+        GL2 gl = drawable.getGL().getGL2();      // get the OpenGL graphics context
+        glu = new GLU();                         // get GL Utilities
+        gl.glClearColor(1.0f, 1.0f, 1.0f, 0.0f); // set background (clear) color
+        gl.glEnable(GL2.GL_DEPTH_TEST); // enables depth testing
+        gl.glHint(GL2.GL_PERSPECTIVE_CORRECTION_HINT, GL2.GL_NICEST); // best perspective correction
+        gl.glShadeModel(GL2.GL_SMOOTH); // blends colors nicely
+    }
+    
+    /**
+     * Handler for window re-size event. Also called when the drawable is
+     * first set to visible
+     */
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+        GL2 gl = drawable.getGL().getGL2();  // get the OpenGL 2 graphics context
+        
+        if (height == 0) height = 1;   // prevent divide by zero
+        float aspect = (float)width / height;
+        
+        //Set the view port (display area) to cover the entire window
+        //gl.glViewport(0, 0, width, height);
+        
+        // Setup perspective projection, with aspect ratio matches viewport
+        gl.glMatrixMode(GL2.GL_PROJECTION);  // choose projection matrix
+        gl.glLoadIdentity();             // reset projection matrix
+        glu.gluPerspective(45.0, aspect, 0.1, 200.0); // fovy, aspect, zNear, zFar
+        
+        // Enable the model-view transform
+        gl.glMatrixMode(GL2.GL_MODELVIEW);
+        gl.glLoadIdentity(); // reset
+    }
+    
+    /**
+     * Called by OpenGL for drawing
+     */
+    public void display(GLAutoDrawable drawable) {
+        GL2 gl = drawable.getGL().getGL2();  // get the OpenGL 2 graphics context
+        GLUT glut = new GLUT();
+        gl.glClear(GL2.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT); // clear color and depth buffers
+        gl.glLoadIdentity();  // reset the model-view matrix
+        
+        // ----- Render figures -----
+        gl.glTranslatef(0.0f, 0.0f, -5.0f);
+        gl.glColor3f(0.0f, 0.0f, 0.5f);
+        glut.glutWireCube(1.0f);
+    }
+    
+    /**
+     * Called before the OpenGL context is destroyed. Release resource such as buffers.
+     */
+    public void dispose(GLAutoDrawable drawable) { }
+    
+    /** The entry main() method to setup the top-level JFrame with our OpenGL canvas inside */
+    public static void main(String[] args) {
+        GLCanvas canvas = new Oppgave2b();
+        canvas.setPreferredSize(new Dimension(CANVAS_WIDTH, CANVAS_HEIGHT));
+        
+        final JFrame frame = new JFrame(); // Swing's JFrame or AWT's Frame
+        frame.getContentPane().add(canvas);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);//stop program
+        frame.setTitle(TITLE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
--- Programmering/Øvinger/Øving13/Transformasjon132a/src/Transformasjon132a.java
+++ Programmering/Øvinger/Øving13/Transformasjon132a/src/Transformasjon132a.java
@@ -1,135 +1,70 @@
-
-import java.awt.*;
-import javax.swing.*;
-
 import com.jogamp.opengl.GL2;
-import com.jogamp.opengl.glu.GLU;
 import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLEventListener;
 import com.jogamp.opengl.awt.GLCanvas;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import com.jogamp.opengl.glu.GLU;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
+import com.jogamp.opengl.util.FPSAnimator;
+
+/**
+ * NeHe Lesson #4 (JOGL 2 Port): Rotation
+ * @author Tomas Holt 2016, based on code by Hock-Chuan Chua
+ * @version May 2012
+ */
+@SuppressWarnings("serial")
 public class Transformasjon132a extends GLCanvas implements GLEventListener {
-    private static String TITLE = "Color cube with triangles";
-    private static final int CANVAS_WIDTH = 640;  // width of the drawable
-    private static final int CANVAS_HEIGHT = 480; // height of the drawable
-    
-    private int rotAngle;
-    
-    //vertices (x,y,z) for each triangle making a nice cube :-)
-    static final float vertices[] = {
-            -1.0f,-1.0f,-1.0f, // triangle 1 : begin
-            -1.0f,-1.0f, 1.0f,
-            -1.0f, 1.0f, 1.0f, // triangle 1 : end
-            1.0f, 1.0f,-1.0f, // triangle 2 : begin
-            -1.0f,-1.0f,-1.0f,
-            -1.0f, 1.0f,-1.0f, // triangle 2 : end
-            1.0f,-1.0f, 1.0f,
-            -1.0f,-1.0f,-1.0f,
-            1.0f,-1.0f,-1.0f,
-            1.0f, 1.0f,-1.0f,
-            1.0f,-1.0f,-1.0f,
-            -1.0f,-1.0f,-1.0f,
-            -1.0f,-1.0f,-1.0f,
-            -1.0f, 1.0f, 1.0f,
-            -1.0f, 1.0f,-1.0f,
-            1.0f,-1.0f, 1.0f,
-            -1.0f,-1.0f, 1.0f,
-            -1.0f,-1.0f,-1.0f,
-            -1.0f, 1.0f, 1.0f,
-            -1.0f,-1.0f, 1.0f,
-            1.0f,-1.0f, 1.0f,
-            1.0f, 1.0f, 1.0f,
-            1.0f,-1.0f,-1.0f,
-            1.0f, 1.0f,-1.0f,
-            1.0f,-1.0f,-1.0f,
-            1.0f, 1.0f, 1.0f,
-            1.0f,-1.0f, 1.0f,
-            1.0f, 1.0f, 1.0f,
-            1.0f, 1.0f,-1.0f,
-            -1.0f, 1.0f,-1.0f,
-            1.0f, 1.0f, 1.0f,
-            -1.0f, 1.0f,-1.0f,
-            -1.0f, 1.0f, 1.0f,
-            1.0f, 1.0f, 1.0f,
-            -1.0f, 1.0f, 1.0f,
-            1.0f,-1.0f, 1.0f};
-    
-    //random colors (RGB) for the vertices
-    static final float colors[] = {
-            0.583f,  0.771f,  0.014f,
-            0.609f,  0.115f,  0.436f,
-            0.327f,  0.483f,  0.844f,
-            0.822f,  0.569f,  0.201f,
-            0.435f,  0.602f,  0.223f,
-            0.310f,  0.747f,  0.185f,
-            0.597f,  0.770f,  0.761f,
-            0.559f,  0.436f,  0.730f,
-            0.359f,  0.583f,  0.152f,
-            0.483f,  0.596f,  0.789f,
-            0.559f,  0.861f,  0.639f,
-            0.195f,  0.548f,  0.859f,
-            0.014f,  0.184f,  0.576f,
-            0.771f,  0.328f,  0.970f,
-            0.406f,  0.615f,  0.116f,
-            0.676f,  0.977f,  0.133f,
-            0.971f,  0.572f,  0.833f,
-            0.140f,  0.616f,  0.489f,
-            0.997f,  0.513f,  0.064f,
-            0.945f,  0.719f,  0.592f,
-            0.543f,  0.021f,  0.978f,
-            0.279f,  0.317f,  0.505f,
-            0.167f,  0.620f,  0.077f,
-            0.347f,  0.857f,  0.137f,
-            0.055f,  0.953f,  0.042f,
-            0.714f,  0.505f,  0.345f,
-            0.783f,  0.290f,  0.734f,
-            0.722f,  0.645f,  0.174f,
-            0.302f,  0.455f,  0.848f,
-            0.225f,  0.587f,  0.040f,
-            0.517f,  0.713f,  0.338f,
-            0.053f,  0.959f,  0.120f,
-            0.393f,  0.621f,  0.362f,
-            0.673f,  0.211f,  0.457f,
-            0.820f,  0.883f,  0.371f,
-            0.982f,  0.099f,  0.879f
-    };
+    // Define constants for the top-level container
+    private static String TITLE = "NeHe Lesson #4: Rotation";
+    private static final int CANVAS_WIDTH = 800;  // width of the drawable
+    private static final int CANVAS_HEIGHT = 640; // height of the drawable
+    private static final int FPS = 60; // animator's target frames per second
     
     // Setup OpenGL Graphics Renderer
+    
     private GLU glu;  // for the GL Utility
+    private float angleTriangle = 0.0f;
+    private float angleQuad = 0.0f;
+    private float speedTriangle = 0.5f;
+    private float speedQuad = -0.4f;
     
     /** Constructor to setup the GUI for this Component */
     public Transformasjon132a() {
         this.addGLEventListener(this);
-        this.addKeyListener(new RotateKeyListener()); //listener for keyboard
     }
-
-// ------ Implement methods declared in GLEventListener (init,reshape,display,dispose)
+    
+    // ------ Implement methods declared in GLEventListener ------
     
     /**
-     * Called immediately after the OpenGL context is initialized. Can be used
+     * Called back immediately after the OpenGL context is initialized. Can be used
      * to perform one-time initialization. Run only once.
      */
+    @Override
     public void init(GLAutoDrawable drawable) {
         GL2 gl = drawable.getGL().getGL2();      // get the OpenGL graphics context
         glu = new GLU();                         // get GL Utilities
         gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // set background (clear) color
-        gl.glEnable(GL2.GL_DEPTH_TEST);           // enables depth testing
+        gl.glClearDepth(1.0f);      // set clear depth value to farthest
+        gl.glEnable(GL2.GL_DEPTH_TEST); // enables depth testing
+        gl.glDepthFunc(GL2.GL_LEQUAL);  // the type of depth test to do
         gl.glHint(GL2.GL_PERSPECTIVE_CORRECTION_HINT, GL2.GL_NICEST); // best perspective correction
+        gl.glShadeModel(GL2.GL_SMOOTH); // blends colors nicely, and smoothes out lighting
     }
     
     /**
-     * Handler for window re-size event. Also called when the drawable is
-     * first set to visible
+     * Call-back handler for window re-size event. Also called when the drawable is
+     * first set to visible.
      */
+    @Override
     public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
         GL2 gl = drawable.getGL().getGL2();  // get the OpenGL 2 graphics context
         
         if (height == 0) height = 1;   // prevent divide by zero
         float aspect = (float)width / height;
         
-        //Set the view port (display area) to cover the entire window
+        // Set the view port (display area) to cover the entire window
         gl.glViewport(0, 0, width, height);
         
         // Setup perspective projection, with aspect ratio matches viewport
@@ -143,49 +78,65 @@ public class Transformasjon132a extends GLCanvas implements GLEventListener {
     }
     
     /**
-     * Called by OpenGL for drawing
+     * Called back by the animator to perform rendering.
      */
+    @Override
     public void display(GLAutoDrawable drawable) {
         GL2 gl = drawable.getGL().getGL2();  // get the OpenGL 2 graphics context
         gl.glClear(GL2.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT); // clear color and depth buffers
+        
+        // ---- Render a Triangle -----
         gl.glLoadIdentity();  // reset the model-view matrix
-        
-        gl.glTranslatef(0f, 0.0f, -6.0f);
-        gl.glRotated(rotAngle, 1, 1, 1);//rotate around x,y,z axis
-        
-        //draw cube by using rectangles (would be easier with line loop/quads)
-        gl.glBegin(GL2.GL_TRIANGLES);
-        for (int i = 0; i < vertices.length/3; i++){
-            int arrayIndex = 3*i;//next "row" in array
-            gl.glColor3fv(colors,arrayIndex);//RGB
-            gl.glVertex3fv(vertices, arrayIndex);
-        }
+        gl.glTranslatef(-1.5f, 0.0f, -6.0f); // translate left and into the screen
+        gl.glRotatef(angleTriangle, 0.0f, 1.0f, 0.0f); // rotate about the y-axis
+        gl.glBegin(GL2.GL_TRIANGLES); // draw using triangles
+        gl.glColor3f(1.0f, 0.0f, 0.0f); // Red
+        gl.glVertex3f(0.0f, 1.0f, 0.0f);
+        gl.glColor3f(0.0f, 1.0f, 0.0f); // Green
+        gl.glVertex3f(-1.0f, -1.0f, 0.0f);
+        gl.glColor3f(0.0f, 0.0f, 1.0f); // Blue
+        gl.glVertex3f(1.0f, -1.0f, 0.0f);
         gl.glEnd();
+        
+        // ---- Render a Quad -----
+        gl.glLoadIdentity(); // reset the current model-view matrix
+        gl.glTranslatef(1.5f, 0.0f, -6.0f); // translate right and into the screen
+        gl.glRotatef(angleQuad, 1.0f, 0.0f, 0.0f); // rotate about the x-axis
+        gl.glColor3f(0.5f, 0.5f, 1.0f); // Light-blue
+        gl.glBegin(GL2.GL_QUADS); // draw using quads
+        gl.glVertex3f(-1.0f, 1.0f, 0.0f);
+        gl.glVertex3f(1.0f, 1.0f, 0.0f);
+        gl.glVertex3f(1.0f, -1.0f, 0.0f);
+        gl.glVertex3f(-1.0f, -1.0f, 0.0f);
+        gl.glEnd();
+        
+        // Update the rotational angle after each refresh.
+        angleTriangle += speedTriangle;
+        angleQuad += speedQuad;
     }
     
     /**
-     * Called before the OpenGL context is destroyed. Release resource such as buffers.
+     * Called back before the OpenGL context is destroyed. Release resource such as buffers.
      */
+    @Override
     public void dispose(GLAutoDrawable drawable) { }
     
-    private class RotateKeyListener extends KeyAdapter{
-        public void keyPressed(KeyEvent e) {
-            //accept any key
-            rotAngle += 10;//
-            Transformasjon132a.this.repaint();//repaint our canvas
-        }
-    }
-    
-    /** The entry main() method to setup the top-level JFrame with our OpenGL canvas inside */
+    /** The entry main() method to setup the top-level container and animator */
     public static void main(String[] args) {
+        // Create the OpenGL rendering canvas
         GLCanvas canvas = new Transformasjon132a();
         canvas.setPreferredSize(new Dimension(CANVAS_WIDTH, CANVAS_HEIGHT));
         
+        // Create a animator that drives canvas' display() at the specified FPS. 
+        final FPSAnimator animator = new FPSAnimator(canvas, FPS, true);
+        
+        // Create the top-level container
         final JFrame frame = new JFrame(); // Swing's JFrame or AWT's Frame
         frame.getContentPane().add(canvas);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);//stop program
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setTitle(TITLE);
         frame.pack();
         frame.setVisible(true);
+        animator.start(); // start the animation loop
     }
 }
