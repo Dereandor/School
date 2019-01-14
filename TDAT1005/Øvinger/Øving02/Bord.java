f9df68556936a36b4eb61acd318c1efa07e8a8c0
Torstein <torstein.holmberget@gmail.com>
2019-01-14 11:40:42 +0100

    2019-01-14

--- TDAT1005/Øvinger/Øving02/Bord.java
+++ TDAT1005/Øvinger/Øving02/Bord.java
@@ -2,10 +2,44 @@ class Bord {
 
     private int available;
     private int busy;
-    private int[] tables;
+    private int total;
+    private String[] tables;
 
-    public Bord() {
+    public Bord(int total) {
+        String[] tables = new String[total];
 
+    }
+
+    public int getAvailable() {
+        available = 0;
+        for(int i = 0; i<tables.length; i++) {
+            if (tables[i].equals(null)) {
+                available++;
+            }
+        }
+        return available;
+    }
+
+    public int getBusy() {
+        busy = 0;
+        for(int i = 0; i<tables.length; i++) {
+            if (!(tables[i].equals(null))) {
+                busy++;
+            }
+        }
+        return busy;
+    }
+
+    //Release cleaned tables
+
+    //Reserve tables
+    public boolean reserveTables(String name, int size) {
+        if (name == null || size = 0) {
+            return false;
+        }
+        for (int i = 0; i < tables.length; i++) {
+            if (tables[i] = null)
+        }
 
     }
 }
--- TDAT1005/Øvinger/Øving02/Restaurant.java
+++ TDAT1005/Øvinger/Øving02/Restaurant.java
@@ -12,34 +12,44 @@ class Restaurant {
     calendar.setTime(date);
     int curYear = calendar.get(Calendar.YEAR);
 
-    public Restaurant(String name, int estYear) {
+    public Restaurant(String name, int estYear, int tables) {
         this.name = name;
         this.estYear = estYear;
+        Bord[] bord = new Bord[tables];
     }
-
+    //get name of restaurant
     public String getName() {
         return name;
     }
-
+    //change name of restaurant
     public void setName(String newName) {
         name = newName;
     }
-
+    //get year of establishment
     public int getYear() {
         return estYear;
     }
-
+    // get age of restaurant
     public int getAge() {
         int age = curYear - estYear;
         return age;
     }
-
+    //find how many available tables in restaurant
     public int getAvailable() {
-        int available = ;
+        int available = bord.getAvailable();
         return available;
     }
-
+    //find how many busy tables in restaurant
     public int getBusy() {
-        return
+        int busy = bord.getBusy();
+        return busy;
     }
+    //reserve a number of tables on name
+    public boolean reserveTables() {
+        if ()
+    }
+    //find which tables a name has reserved, returned as int[]
+
+
+    //release cleaned tables, int[] argument
 }
