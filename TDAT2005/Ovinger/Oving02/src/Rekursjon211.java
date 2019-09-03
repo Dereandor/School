2ebef481410e469d59371cce39d1ed0bea4c9123
Torstein <torstein.holmberget@gmail.com>
2019-09-03 10:14:19 +0200

    completed task 2

--- TDAT2005/Ovinger/Oving02/src/Rekursjon211.java
+++ TDAT2005/Ovinger/Oving02/src/Rekursjon211.java
@@ -8,23 +8,47 @@ public class Rekursjon211 {
         }
         return x * rekursjonEnkel((x), (n-1));
     }
+    
+    private static int ytelseRekursjonEnkel(double x, int n, int testrunder) {
+        int runder = 0;
+        long start = System.currentTimeMillis();;
+        long tidsbruk = 0;
+        long slutt;
+        int resultat;
+        do {
+            resultat = (int) rekursjonEnkel(x, n);
+            slutt = System.currentTimeMillis();
+            runder++;
+            tidsbruk += (slutt - start);
+        } while(tidsbruk < testrunder);
+        double tid = (double) (tidsbruk / runder);
+        System.out.println("Millisekund pr. runde (rekursjon): "+ tid);
+        return resultat;
+    }
+    
+    private static int ytelseMathPow(double x, int n, int testrunder){
+        int runder = 0;
+        long start = System.currentTimeMillis();;
+        long tidsbruk = 0;
+        long slutt;
+        int resultat;
+        do {
+            resultat = (int) Math.pow(x, n);
+            slutt = System.currentTimeMillis();
+            runder++;
+            tidsbruk += (slutt - start);
+        } while(tidsbruk < testrunder);
+        double tid = (double) (tidsbruk / runder);
+        System.out.println("Millisekund pr. runde (Math.pow): "+ tid);
+        return resultat;
+    }
 
     public static void main(String[] args) {
-
-        double x = 1.1001;
-        int n = 5000;
+    
+        double x = 1.01;
+        int n = 10000;
         int testrunder = 100000;
-
-        Date start = new Date();
-        int runder = 0;
-        double tid;
-        Date slutt;
-        do {
-            rekursjonEnkel(x, n);
-            slutt = new Date();
-            ++runder;
-        } while (slutt.getTime() - start.getTime() < 1000);
-        tid = (double) (slutt.getTime() - start.getTime()) / runder;
-        System.out.println("Millisekund pr. runde:" + tid);
+        System.out.println("Resultat: "+ytelseRekursjonEnkel(x, n, testrunder));
+        System.out.println("Resultat: "+ytelseMathPow(x, n, testrunder));
     }
 }
--- TDAT2005/Ovinger/Oving02/src/Rekursjon223.java
+++ TDAT2005/Ovinger/Oving02/src/Rekursjon223.java
@@ -1,6 +1,52 @@
 import java.util.Date;
 
 public class Rekursjon223 {
+    
+    private static double rekursjonAvansert(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if ((n % 2)== 0) {
+            return rekursjonAvansert((x*x), (n/2));
+        }
+        else {
+            return x * rekursjonAvansert((x*x), ((n-1) / 2));
+        }
+    }
+    
+    private static int ytelseRekursjonAvansert(double x, int n, int testrunder){
+        int runder = 0;
+        long start = System.currentTimeMillis();
+        long tidsbruk = 0;
+        long slutt;
+        int resultat;
+        do {
+            resultat = (int) rekursjonAvansert(x, n);
+            slutt = System.currentTimeMillis();
+            runder++;
+            tidsbruk += (slutt - start);
+        } while(tidsbruk < testrunder);
+        double tid = (double) (tidsbruk / runder);
+        System.out.println("Millisekund pr. runde (rekursjon): "+ tid);
+        return resultat;
+    }
+    
+    private static int ytelseMathPow(double x, int n, int testrunder){
+        int runder = 0;
+        long start = System.currentTimeMillis();
+        long tidsbruk = 0;
+        long slutt;
+        int resultat;
+        do {
+            resultat = (int) Math.pow(x, n);
+            slutt = System.currentTimeMillis();
+            runder++;
+            tidsbruk += (slutt - start);
+        } while(tidsbruk < testrunder);
+        double tid = (double) (tidsbruk / runder);
+        System.out.println("Millisekund pr. runde (Math.pow): "+ tid);
+        return resultat;
+    }
 
 
     public static void main(String[] args) {
@@ -8,17 +54,8 @@ public class Rekursjon223 {
         double x = 1.1001;
         int n = 5000;
         int testrunder = 100000;
-
-        Date start = new Date();
-        int runder = 0;
-        double tid;
-        Date slutt;
-        do {
-            //method here
-            slutt = new Date();
-            ++runder;
-        } while (slutt.getTime() - start.getTime() < 1000);
-        tid = (double) (slutt.getTime() - start.getTime()) / runder;
-        System.out.println("Millisekund pr. runde:" + tid);
+    
+        System.out.println("Resultat: "+ytelseRekursjonAvansert(x, n, testrunder));
+        System.out.println("Resultat: "+ytelseMathPow(x, n, testrunder));
     }
 }
