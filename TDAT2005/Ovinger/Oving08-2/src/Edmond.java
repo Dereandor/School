ee63e7be81dc61e9dca135b12dff93f854adb997
Dereandor <torstein.holmberget@gmail.com>
2019-10-28 10:20:10 +0100

    øving 8 tdat205

--- TDAT2005/Ovinger/Oving08-2/src/Edmond.java
+++ TDAT2005/Ovinger/Oving08-2/src/Edmond.java
@@ -1,28 +1,26 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Edmond{
+public class Edmond {
 
-    public static ArrayList<Integer> reverseArrayList(ArrayList<Integer> alist)
-    {
+    public static ArrayList<Integer> reverseArrayList(ArrayList<Integer> alist) {
         ArrayList<Integer> revArrayList = new ArrayList<Integer>();
         for (int i = alist.size() - 1; i >= 0; i--) {
 
             revArrayList.add(alist.get(i));
         }
-        for (int i = 0; i < revArrayList.size()-1; i++) {
-            if (revArrayList.get(i).equals(revArrayList.get(i+1))) {
+        for (int i = 0; i < revArrayList.size() - 1; i++) {
+            if (revArrayList.get(i).equals(revArrayList.get(i + 1))) {
                 revArrayList.remove(i);
             }
         }
         return revArrayList;
     }
 
-    public static void main(String[]args) throws Exception {
-        String filepath = "C:\\School\\TDAT2005\\Ovinger\\Oving08-2\\flytgraf1.txt";
+    public static void main(String[] args) throws Exception {
+        String filepath = "C:\\School\\TDAT2005\\Ovinger\\Oving08-2\\flytgraf3.txt";
 
         BufferedReader br = new BufferedReader(new FileReader(filepath));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -30,7 +28,7 @@ public class Edmond{
         int nodes = Integer.parseInt(st.nextToken());
         int edges = Integer.parseInt(st.nextToken());
         int source = 0;
-        int sink = nodes -1;
+        int sink = 1;
 
         Node[] graph = new Node[nodes];
 
@@ -44,10 +42,10 @@ public class Edmond{
             int v = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
 
-            Edge a = new Edge(u, v, 0, c);
-            Edge b = new Edge(v, u, 0, 0);
+            Edge a = new Edge(u, v, 0, c); //TODO sjekk om finnes fra før
+            Edge b = new Edge(v, u, 0, 0); //TODO sjekk om finnes fra før
 
-            a. setReverse(b);
+            a.setReverse(b);
             b.setReverse(a);
 
             graph[u].edges.add(a);
@@ -56,40 +54,41 @@ public class Edmond{
 
         int maxFlow = 0;
 
-        while(true) {
+        while (true) {
             Edge[] parent = new Edge[nodes];
 
             ArrayList<Node> q = new ArrayList<>();
             q.add(graph[source]);
 
-            //BFS
-            while(!q.isEmpty()) {
+            // BFS
+            while (!q.isEmpty()) {
                 Node curr = q.remove(0);
 
                 for (Edge e : curr.edges) {
-                    if(parent[e.v] == null && e.v != source && e.capacity > e.flow) {
+                    if (parent[e.v] == null && e.v != source && e.capacity > e.flow) {
                         parent[e.v] = e;
                         q.add(graph[e.v]);
                     }
                 }
             }
 
-            //sluk ikke nådd, hopper ut av løkken.
-            if (parent[sink] == null) break;
+            // sluk ikke nådd, hopper ut av løkken.
+            if (parent[sink] == null)
+                break;
 
-            //sluk nådd, øker flytverdi
+            // sluk nådd, øker flytverdi
             int pushFlow = Integer.MAX_VALUE;
 
-            //finner maxflyt
+            // finner maxflyt
             for (Edge e = parent[sink]; e != null; e = parent[e.u]) {
                 pushFlow = Math.min(pushFlow, e.capacity - e.flow);
 
             }
 
-            //legger til flytverdier
+            // legger til flytverdier
             System.out.print("Økning: " + pushFlow + " ");
             ArrayList<Integer> flowIncrease = new ArrayList<>();
-            for (Edge e = parent[sink];e != null; e = parent[e.u]) {
+            for (Edge e = parent[sink]; e != null; e = parent[e.u]) {
                 e.flow += pushFlow;
                 e.reverse.flow -= pushFlow;
                 flowIncrease.add(e.v);
@@ -98,7 +97,7 @@ public class Edmond{
             for (int i = 0; i < flowIncrease.size(); i++) {
 
             }
-             flowIncrease = reverseArrayList(flowIncrease);
+            flowIncrease = reverseArrayList(flowIncrease);
             System.out.println(" gjennom: " + flowIncrease.toString());
 
             maxFlow += pushFlow;
